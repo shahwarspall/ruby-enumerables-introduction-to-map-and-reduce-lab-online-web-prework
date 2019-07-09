7b7847c5f95593d85ba645a966d4696c2e2e80aa
Shahwar Spall <shevy.spall@gmail.com>
2019-07-09 04:05:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,7 +53,7 @@ end
 def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.length do
-    if source_array[i] == "true"
+    if source_array[i] == true
       return true
     end
     i += 1 
