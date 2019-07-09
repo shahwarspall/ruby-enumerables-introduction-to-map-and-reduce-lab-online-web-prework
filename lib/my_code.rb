fce9c2ca2e36dcc222df4fca796aa2d76822879c
Shahwar Spall <shevy.spall@gmail.com>
2019-07-09 03:21:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# My Code here....
+def map_to_negativize(source_array)
+  i = 0
+  while i < source_array.length do
+   source_array[i] *= -1
+   i += 1 
+end
+ source_array
+ end
+ 
+ def map_to_no_change(source_array)
+   i = 0 
+   while i < source_array.length do
+     source_array[i]
+     i += 1 
+   end
+   source_array
+ end
+ 
+ def map_to_double(source_array)
+   i = 0 
+   while i < source_array.length do
+     source_array[i] *= 2 
+     i += 1 
+   end
+   source_array
+ end
+ 
+ 
